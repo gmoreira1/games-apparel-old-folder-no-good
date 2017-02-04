0d7be969e5177674987f2f62c8a495b96cbd8c46
gmoreira1 <g_moreira@fanshaweonline.ca>
2017-02-03 20:56:18 -0500

    Add files via upload

---
+++ Kesper5/script/contentObject.js
@@ -0,0 +1,40 @@
+var dynamicCon = {
+
+		home: {
+			text : "------------------",	
+
+			headline : "Welcome to Kesper Gaming Apparel"
+		},
+
+		team : {
+			text : "Our team is unbeatable, unstoppable, we are a force to be reckon with",
+
+			headline : "There's no I in TEAM"
+		},
+
+		merch : {
+			text : "Check out our merch and then buy it!",
+
+			headline : "Merchandise"
+		},
+
+		home1: {	
+		
+			text : "------------------",		
+
+			headline : "Welcome to Kesper Gaming Apparel"
+		},
+
+		team2 : {
+			text : "Our team is unbeatable, we are a force to be reckon with.",
+
+			headline : "There's no I in TEAM"
+		},
+
+		merch3 : {
+			text : "Check out our merch and then buy it!",
+
+			headline : "Merchandise"
+		}
+		
+	};
